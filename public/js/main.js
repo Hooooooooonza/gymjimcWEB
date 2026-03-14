1f08ae79c7ed66cfb423bbcf9ec93b89f16dd4dc
Hooooooooonza <lukasj@jirovcovka.net>
2026-03-14 17:49:05 +0100

    Change server status check interval to 10ms

--- public/js/main.js
+++ public/js/main.js
@@ -93,7 +93,7 @@ async function checkServerStatus() {
 
 document.addEventListener('DOMContentLoaded', () => {
   checkServerStatus();
-  setInterval(checkServerStatus, 10000);
+  setInterval(checkServerStatus, 10);
 });
 
 // ── Init ──
